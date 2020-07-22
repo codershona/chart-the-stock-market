48d01b1fdc968ffb31671e6f08ccad9741820e86
codershona <falgunimst95@gmail.com>
2020-07-22 12:56:12 +0200

    working on api server

--- server/routes/api/stock.js
+++ server/routes/api/stock.js
@@ -1,24 +1,47 @@
+const request = requiure('superagent');
+
+
+
+
 module.exports = (app) => {
 
   app.post('/api/stock/portfolio', function (req, res, next) {
+
+    const apiKey = 'demo';
     
      // Assuming looking up the user:
 
      // Retrieve ticker symbols:
 
-     const tickers = [];
+     const tickers = ['MSFT'];
+
+     let completed = 0;
 
      // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo
+     
+     for (let i =0; i < tickers.length; i += 1) {
+
 
       request
    // .get('/search')
    .get('https://www.alphavantage.co/query')
-   .query({ query: 'Manny' })
-   .query({ range: '1..5' })
-   .query({ order: 'desc' })
-   .then(res => {
+   .query({ 'function': 'TIME_SERIES_DAILY' })
+   .query({ apikey: apiKey })
+   .query({ symbol: ticker })
+   .then((res) => {
+    completed += 1;
+    // if (completed === i) {
+       if (completed === tickers.length) {
 
-   });
+      // All tickers have finished their requests
+
+    }
+
+    // completed += 1;
+
+     });
+
+  }
 
 //  working:
 
